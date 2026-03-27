e3e83b36708a7a236da335e39b1c45c111e70de9
Ayaan Amjad <ayaanquasmi2@eng.rizvi.edu.in>
2026-03-27 11:43:03 +0530

    WIP: partial vendor-app-ui changes - review needed

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,11 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
-const { withNativeWind } = require('nativewind/metro');
+const { withNativeWind } = require("nativewind/metro");
 
+// Get the default config from Sentry
 const config = getSentryExpoConfig(__dirname);
 
 // Wrap the config with NativeWind
-module.exports = withNativeWind(config, { 
-    input: './global.css' 
+module.exports = withNativeWind(config, {
+  input: "./global.css",
 });
